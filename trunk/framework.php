935967e2fd38e415491a1abf097aee6bea8f66e6
sakiv <sakiv@60e4c770-6527-de5a-69b5-90506f80320e>
2011-08-02 05:45:00 +0000

    Updated code to add comments and TODOs.

--- trunk/framework.php
+++ trunk/framework.php
@@ -11,6 +11,11 @@ if(!defined('sakiv.framework')){die('External Access to File Denied');}
  */
 define('SF_CONFIG_FILE_NAME', 'framework.inc.php');
 
+/**
+ * Sakiv.Framework entry point class.
+ * @author sakiv
+ *
+ */
 class sf {
 
 	/**
@@ -19,53 +24,104 @@ class sf {
 	 */
 	public static function loadFramework($configPath = null) {
 
+		// Verify if configPath is passed or not
+		// If not, then try to locate config file using locateConfigFile()
 		if (!isset($configPath)) {
 			$configPath = self::locateConfigFile();
 		}
 
+		// If configPath is still not set then stop processing
 		if(!isset($configPath))
 			die("Could not find Sakiv Framework config file ['$configPath']");
 
-		return self::load($configPath);
+		// TODO: Try to use ErrorHandler or some common thing here.
 
+		return self::load($configPath);
 	}
 
-	private static function locateConfigFile() {
+	/**
+	 * Locates Sakiv Framework config file within the given path
+	 * @return string
+	 */
+	private static function locateConfigFile($lookupPath = null) {
 
 		/**
 		* Sakiv.Framework: List of path to locate the framework config file.
 		* @var unknown_type
 		*/
-		$configLookupPath = array('./', './config');
+		$defaultLookupPath = array('./', './config');
 
-		foreach ($configLookupPath as $path) {
+		// Verify if lookupPath is passed or not
+		// If passed, then process it based on its type
+		if(isset($lookupPath)) {
+			switch (gettype($lookupPath)) {
+				// If array then merge it into defaultLookupPath
+				case 'array':
+					$defaultLookupPath = array_merge($lookupPath, $defaultLookupPath);
+					break;
+
+				// If string then add it to beginning of defaultLookupPath
+				case 'string':
+					array_unshift($defaultLookupPath, $lookupPath);
+					break;
+
+				// Print warning for any other data type
+				default:
+					print_r("warning: Lookup path ['$lookupPath'] not supported.");
+					break;
+
+			}
+		}
+
+		// Try to locate framework config file in each of the lookup Path
+		foreach ($defaultLookupPath as $path) {
 			$path = self::joinPath($path,SF_CONFIG_FILE_NAME);
+			// If file found then return the path
 			if (file_exists($path)) {
 				return $path;
 			}
 		}
 	}
 
+
+	/**
+	 * Joins the given path by eliminating any extra forward slashes "/".
+	 * @param string $a
+	 * @param string $b
+	 * @return string
+	 */
 	private static function joinPath($a,$b) {
 		return rtrim($a, '/') .'/'. ltrim($b, '/');
 	}
 
+	/**
+	 * Loads the framework using framework config file from given path.
+	 * @param unknown_type $configPath
+	 */
 	private static function load($configPath) {
 
 		// Start buffer
 		ob_start();
 
+		// Define the version for framework
 		define('SF_VERSION', '0.1b');
 
+		// If framework path is not defined then set to default value
 		if(!defined(SF_PATH))
 			define('SF_PATH', './framework');
 
+		// Load the framework config file
 		require_once $configPath;
 
-		// Load core library
-
+		// Validate framework path or stop processing
 		if(!file_exists(SF_PATH))
 			die("Could not find Sakiv Framework path ['SF_PATH']");
+
+		// TODO: Try to use ErrorHandler here.
+
+		////////////////////////////////////////////////////////////
+		// Load core library
+		////////////////////////////////////////////////////////////
 
 		$sf_core = self::joinPath(SF_PATH, 'core');
 		if($handle = opendir($sf_core)) {
@@ -87,6 +143,7 @@ class sf {
 
 			closedir($handle);
 		}
+		////////////////////////////////////////////////////////////
 
 		// Release the output
 		ob_end_flush();
